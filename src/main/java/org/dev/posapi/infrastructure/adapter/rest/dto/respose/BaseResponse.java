c5f75379e4ad75bf66577ad532ea27bc19ebeade
Mauricio Cruz <mauricioacp34@gmail.com>
2025-01-19 15:01:22 -0600

    Add BaseResponse DTO for standardized API responses

---
+++ src/main/java/org/dev/posapi/infrastructure/adapter/rest/dto/respose/BaseResponse.java
@@ -0,0 +1,21 @@
+package org.dev.posapi.infrastructure.adapter.rest.dto.respose;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Builder
+@Getter
+@Setter
+public class BaseResponse {
+    private HttpStatus status;
+    private Integer statusCode;
+    private String message;
+    private Boolean success;
+    private Object data;
+    public ResponseEntity<BaseResponse> apply() {
+        return new ResponseEntity<>(this, status);
+    }
+}
